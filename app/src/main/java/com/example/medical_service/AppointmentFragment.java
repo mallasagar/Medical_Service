55323669c66d4093a48abf1fb869ea5065e764d9
mallasagar <mallasagar0101@gmail.com>
2021-05-31 20:48:52 +0545

    added fragment

---
+++ app/src/main/java/com/example/medical_service/AppointmentFragment.java
@@ -0,0 +1,18 @@
+package com.example.medical_service;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class AppointmentFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_appointment,container,false);
+    }
+}
--- app/src/main/java/com/example/medical_service/MainActivity.java
+++ app/src/main/java/com/example/medical_service/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.medical_service;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,8 +8,11 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
@@ -16,16 +20,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        navigation bar start
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
         ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this,drawer,toolbar,
                 R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toogle);
         toogle.syncState();
 
 
+        if (savedInstanceState ==null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new MedecineFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_medecine);
+        }
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull  MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.nav_medecine:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MedecineFragment()).commit();
+                break;
+            case R.id.nav_appointment:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                       new AppointmentFragment()).commit();
+                break;
+            case R.id.nav_doctor:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new DoctorFragment()).commit();
+                break;
+            case R.id.nav_emergency:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new EmergencyFragment()).commit();
+                break;
+            case R.id.nav_login:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new LoginFragment()).commit();
+                break;
+            case R.id.nav_about:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AboutFragment()).commit();
+                break;
+
+
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
@@ -36,4 +90,5 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+    //navigation bar end
 }
